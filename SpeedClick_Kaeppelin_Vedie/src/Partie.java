8f8f5756fecd71c509ea76560a0be8f191962003
vedie <vedie@192.168.115.87>
2021-12-10 12:19:18 +0100

    partie class

---
+++ SpeedClick_Kaeppelin_Vedie/src/Partie.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vedie
+ */
+public class Partie {
+    Grille GrilleJeu=new Grille();
+}
