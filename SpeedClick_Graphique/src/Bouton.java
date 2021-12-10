34dad8e3e3690ebd3dca456ec34576eec02bfda6
vedie <vedie@192.168.115.87>
2021-12-10 12:15:48 +0100

    graphique créé

---
+++ SpeedClick_Graphique/src/Bouton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author KAEPPELIN Mayeul
+ */
+public class Bouton {
+    Bouton boutonCourant;
+    String Couleur ;
+    
+    
+    public void Cellule(){
+        boutonCourant = null;
+        Couleur = "noir";
+        
+    }
+}
