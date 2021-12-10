2b89615019a551ead8c094c9d3897f66ae5e7bb4
KAEPPELIN Mayeul <KAEPPELIN Mayeul@LAPTOP-C2UTJ6LC>
2021-12-10 11:54:45 +0100

    création du projet

--- SpeedClick_Kaeppelin_Vedie/src/speedclick_kaeppelin_vedie/SpeedClick_Kaeppelin_Vedie.java
+++ SpeedClick_Kaeppelin_Vedie/src/speedclick_kaeppelin_vedie/SpeedClick_Kaeppelin_Vedie.java
@@ -14,7 +14,7 @@ public class SpeedClick_Kaeppelin_Vedie {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
