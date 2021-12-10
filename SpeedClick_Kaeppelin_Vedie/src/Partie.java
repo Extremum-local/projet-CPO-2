e302f7cadc63c651cb7dbacbcc49fe9c73f5e144
vedie <vedie@192.168.115.87>
2021-12-10 15:28:44 +0100

    Classe partie commencée (initialiser partie)

--- SpeedClick_Kaeppelin_Vedie/src/Partie.java
+++ SpeedClick_Kaeppelin_Vedie/src/Partie.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.Random;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,13 +13,27 @@ import java.util.Scanner;
  
 public class Partie {
     Grille GrilleJeu=new Grille();
+    Joueur JoueurCourant;
+    
    
+    public void initialiserPartie() {
+       
+        GrilleJeu.eteindreGrille(); //on eteint toute la grille avant chaque début de partie
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Pseudo: "); //on demande au joueur de rentrer son nom
+        JoueurCourant = new Joueur(sc.nextLine());
+        
+        Random r = new Random();
+        int lig1 = r.nextInt(4); //on choisit un nb entre 1 et 4 pour définir le bouton qui va s'allumer dès le début
+        int col1 = r.nextInt(4);
+        GrilleJeu.AllumerBouton(lig1, col1); //des le début, on aura un de nos boutons allumé.
+    }
+    
+    
     
     public void debuterPartie() {
         
-        Scanner sca = new Scanner (System.in);
-        String nomJoueur = sca.next();
-        Joueur J = new Joueur (nomJoueur);
+        
         
         
         
